18303f46ecd54f5a74cc98d4bf234947af7171bf
CodeLyricist <face.of.fury@gmail.com>
2019-08-10 21:54:26 -0400

    first commit
    first commit

---
+++ Test Suites/Suite - Go to Youtube.ts
@@ -0,0 +1,19 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>Suite - Go to Youtube</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <testSuiteGuid>1c9bdb49-7ae7-40a4-a904-c8b40aae3cde</testSuiteGuid>
+   <testCaseLink>
+      <guid>942f958e-0424-45e1-a708-cf9b37a22ddd</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Go To Youtube Page</testCaseId>
+   </testCaseLink>
+</TestSuiteEntity>
