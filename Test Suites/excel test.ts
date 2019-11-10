1b01e5272b744abebe8584b071a65b380d9bdc63
CodeLyricist <face.of.fury@gmail.com>
2019-11-10 18:31:31 -0500

    added test suite

---
+++ Test Suites/excel test.ts
@@ -0,0 +1,19 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>excel test</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <testSuiteGuid>fac86c4f-ab29-4a90-99e7-9dc58150670b</testSuiteGuid>
+   <testCaseLink>
+      <guid>e0c52137-d82c-4f3d-af3d-3810b4ecee01</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Excel Test</testCaseId>
+   </testCaseLink>
+</TestSuiteEntity>
